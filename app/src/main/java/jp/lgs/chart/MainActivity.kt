bc470013ea0a12e19ad699807d80fe532c12c438
kuroyan <m_kurosaki@mtf.biglobe.ne.jp>
2020-03-27 11:45:15 +0900

    グラフの初期化をしてビルドして暴走しなかった版。描画ないので設定結果が正しいかは分からない

--- app/src/main/java/jp/lgs/chart/MainActivity.kt
+++ app/src/main/java/jp/lgs/chart/MainActivity.kt
@@ -1,12 +1,70 @@
 package jp.lgs.chart
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.mikephil.charting.components.Legend
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // スタイルとフォントファミリーの設定
+    private var mTypeface = Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL)
+    // データの個数
+    private val chartDataCount = 20
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // グラフの設定
+        setupLineChart()
+        // データの設定
+        // lineChart.data = lineData(chartDataCount, 100f)
     }
+
+    private fun setupLineChart(){
+        lineChart.apply {
+            description.isEnabled = false
+            setTouchEnabled(true)
+            isDragEnabled = true
+            // 拡大縮小可能
+            isScaleXEnabled = true
+            setPinchZoom(false)
+            setDrawGridBackground(false)
+
+            //データラベルの表示
+            legend.apply {
+                form = Legend.LegendForm.LINE
+                typeface = mTypeface
+                textSize = 11f
+                textColor = Color.BLACK
+                verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+                horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+                orientation = Legend.LegendOrientation.HORIZONTAL
+                setDrawInside(false)
+            }
+
+            //y軸右側の設定
+            axisRight.isEnabled = false
+
+            //X軸表示
+            xAxis.apply {
+                typeface = mTypeface
+                setDrawLabels(false)
+                // 格子線を表示する
+                setDrawGridLines(true)
+            }
+
+            //y軸左側の表示
+            axisLeft.apply {
+                typeface = mTypeface
+                textColor = Color.BLACK
+                // 格子線を表示する
+                setDrawGridLines(true)
+            }
+        }
+    }
+
 }
